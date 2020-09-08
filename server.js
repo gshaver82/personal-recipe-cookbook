a2199563d117242d75262f538d77655a823e4cff
gshaver <geneshaver@gmail.com>
2020-09-08 17:12:14 -0500

    Update server.js

--- server.js
+++ server.js
@@ -26,6 +26,18 @@ mongoose.connect(
     }
 );
 
+
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://webaccessusername:bootstrapvegetable@atlascluster.5pnpg.mongodb.net/<dbname>?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     // perform actions on the collection object
+//     client.close();
+// });
+
+
 app.use(routes);
 
 app.get("*", function (req, res) {
