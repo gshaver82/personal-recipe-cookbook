5d7df155ad8445bb43f61454c79cb90565be37a6
gshaver <geneshaver@gmail.com>
2020-09-12 11:14:06 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,31 @@ const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
 const admin = require('firebase-admin');
-// const serviceAccount = require("../config/serviceAccountKey.json");
+
+
+try {
+    const serviceAccount = require("../config/serviceAccountKey.json");
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: "https://recipe-box-6f07a.firebaseio.com"
+    });
+
+} catch (error) {
+    console.error(error);
+    admin.initializeApp({
+        credential: admin.credential.cert({
+            "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+            "private_key": process.env.FIREBASE_PRIVATE_KEY,
+            "project_id": process.env.FIREBASE_PROJECT_ID,
+        }),
+        databaseURL: "https://recipe-box-6f07a.firebaseio.com"
+    });
+
+}
+
 
 
 // Initialize Firebase Admin
-admin.initializeApp({
-    credential: admin.credential.cert( process.env.FIREBASE_CLIENT_EMAIL ? {
-        "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-        "private_key": process.env.FIREBASE_PRIVATE_KEY,
-        "project_id": process.env.FIREBASE_PROJECT_ID,
-    } : serviceAccount),
-    databaseURL: "https://recipe-box-6f07a.firebaseio.com"
-});
 
 // This function might be able to live in apiRoutes.js. Let me know if you'd rather have it there, or feel free to move it.
 function checkAuth(req, res, next) {
